e8cead82598cb29f2269528ae77331d16408e66b
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-09-27 00:40:13 +0000

    edited code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,105 +1,36 @@
 #!/usr/bin/python3
+"""0-island_perimeter.py
+
+This module implements the island_perimeter function that
+calculates the perimeter of an island.
 """
-Module that contains various functions that
-allow to play the Prime Game
-"""
 
 
-def bsd_rand(seed):
+def island_perimeter(grid):
+    """island_perimeter function
+    This function calculates the perimeter of an island.
+    Args:
+        grid (list of lists): A grid of integers.
+    Returns:
+        int: The perimeter of the island.
     """
-    decorator for randomizing
-    """
-    def rand():
-        """
-        the seed
-        """
-        nonlocal seed
-        seed = (1103515245*seed + 12345) & 0x7fffffff
-        return seed
-    return rand
-
-
-rand = bsd_rand(32)
-
-
-def isWinner(x, nums):
-    """
-    Return winner from  the game
-    """
-    if x <= 0 or x > 10000:
-        return None
-    players_dict = {'Maria': 0, 'Ben': 0}
-    players = ['Maria', 'Ben']
-    players_dict_winner = {'Maria': 0, 'Ben': 0}
-    if x == len(nums):
-        for number in nums:
-            consecutive_int = [i for i in range(1, number + 1)]
-            recursive_play(players[:], players_dict, consecutive_int)
-            maria_wins = players_dict['Maria']
-            ben_wins = players_dict['Ben']
-            if maria_wins == 1:
-                players_dict_winner['Maria'] += 1
-            if ben_wins == 1:
-                players_dict_winner['Ben'] += 1
-    else:
-        while x != 0:
-            for number in nums:
-                consecutive_int = [i for i in range(1, number + 1)]
-                recursive_play(players[:], players_dict, consecutive_int)
-                maria_wins = players_dict['Maria']
-                ben_wins = players_dict['Ben']
-                if maria_wins == 1:
-                    players_dict_winner['Maria'] += 1
-                if ben_wins == 1:
-                    players_dict_winner['Ben'] += 1
-            x -= 1
-    if players_dict_winner['Maria'] == players_dict_winner['Ben']:
-        winner = None
-    elif players_dict_winner['Maria'] > players_dict_winner['Ben']:
-        winner = 'Maria'
-    else:
-        winner = 'Ben'
-    return winner
-
-
-def recursive_play(players, players_dict, consecutive_int):
-    """
-    recursive helper function on the game
-    """
-    if len(consecutive_int) == 0:
-        return
-    toggle_player(players, players_dict)
-    prime_numbers = []
-    for number in consecutive_int:
-        if check_prime(number):
-            prime_numbers.append(number)
-    if len(prime_numbers) == 0:
-        toggle_player(players, players_dict)
-        return
-    prime_pick = prime_numbers[rand() % len(prime_numbers)]
-    consecutive_int.remove(prime_pick)
-    for number in consecutive_int[:]:
-        if number % prime_pick == 0 and number in consecutive_int:
-            consecutive_int.remove(number)
-    recursive_play(players, players_dict, consecutive_int)
-
-
-def check_prime(number):
-    """check's if number is prime or not"""
-    if number == 1:
-        return False
-    primes = [2, 3, 5, 7]
-    for p in primes:
-        if number % p == 0 and p != number:
-            return False
-    return True
-
-
-def toggle_player(players, players_dict):
-    """
-    toggle current player
-    """
-    player = players.pop(0)
-    players_dict[player] = 1
-    players_dict[players[-1]] = 0
-    players.append(player)
+    count = 0
+    if grid is None:
+        return 0
+    if len(grid) == 0:
+        return 0
+    for row in grid:
+        if len(row) == 0:
+            return 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    count += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    count += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    count += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    count += 1
+    return count
